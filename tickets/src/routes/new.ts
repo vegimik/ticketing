d2702be1685f514569dbbc1f741f3d7b7cd262b9
Vegim Karaçica <vegimi.k@hotmail.com>
2022-09-09 00:22:42 +0200

    Injected lib. @wegotickets/common@1.0.15 managing NATS Client

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -3,6 +3,8 @@ import { preProcessFile } from "typescript";
 import { requireAuth, validateRequest } from "@wegotickets/common";
 import { body } from "express-validator";
 import { Ticket } from "../models/ticket";
+import TicketCreatedPublisher from "../events/publishers/ticket-created-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -25,9 +27,16 @@ router.post(
       userId: req.currentUser!.id,
     });
     await ticket.save();
+
+    new TicketCreatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
+
     res.status(201).send(ticket);
   }
 );
-
 
 export { router as CreateTicketRouter };
